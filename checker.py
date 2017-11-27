7a4135095d6eaa7bc04407fa927b2c47f8ceb591
Alex Kaiser <alextkaiser@gmail.com>
2017-11-26 23:28:54 -0800

    Adding the ability to send email if a check fails

--- checker.py
+++ checker.py
@@ -4,23 +4,60 @@ from checkers import RandomChecker
 
 OUTPUT_DIR = "/home/akaiser/checker"
 
-# Add checks
-# add check
+GMAIL_USR = ""
+GMAIL_PWD = ""
+CREDENTIALS_FILE = "/home/akaiser/dev/checker/credentials.txt"
+with open(CREDENTIALS_FILE, "r") as f:
+    lines = f.readlines()
+    GMAIL_USR = lines[0].strip()
+    GMAIL_PWD = lines[1].strip()
 
 
-def check_disk_space():
-    return (True, "")
+def send_email(recipient, subject, body):
+    import smtplib
+
+    FROM = GMAIL_USR
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(GMAIL_USR, GMAIL_PWD)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print("successfully sent the mail")
+    except:
+        print("Failed to send mail.")
 
 
 if __name__ == "__main__":
 
     checks = []
-    checks.append(RandomChecker())
+    checks.append([RandomChecker(), False])
 
     while True:
         print("Starting check loop")
-        for check in checks:
-            check_output = check.runCheck()
+        for check_tuple in checks:
+            check = check_tuple[0]
+            name = check.get_name()
+            check_output = check.run_check()
             if not check_output[0]:
                 print("Check failed: {}".format(check_output[1]))
+                if not check_tuple[1]:
+                    send_email("alextkaiser@gmail.com",
+                               "{} check failed".format(name),
+                               check_output[1])
+                    check_tuple[1] = True
+            else:
+                if check_tuple[1]:
+                    send_email("alextkaiser@gmail.com",
+                               "{} check recovered".format(name),
+                               "{} is back to normal.".format(name))
+                    check_tuple[1] = False
         time.sleep(10)
--- checkers.py
+++ checkers.py
@@ -13,10 +13,14 @@ class CheckerClass:
     Interface to implement for a checker
     '''
 
-    def runCheck(self):
+    def run_check(self):
         '''
         Method to implement for a check, should return a tuple of (boolean, string)
         '''
+        pass
+
+    def get_name(self):
+        pass
 
 
 FAILURE_RATE = 50
@@ -27,9 +31,12 @@ class RandomChecker(CheckerClass):
     Checker that fails FAILURE_RATE percent of the time
     '''
 
-    def runCheck(self):
+    def run_check(self):
         random_var = random.random()
         if (random_var < (FAILURE_RATE / 100.0)):
             return (False, "Random var was: {}".format(random_var))
         else:
             return (True, "")
+
+    def get_name(self):
+        return "Random check"
